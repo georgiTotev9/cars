9d7ce3246c367f529fce62b560305c4ca6c3a542
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-03-09 15:49:23 +0200

    added routing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// [ ] initialize and configure Express app
-// [ ] initialize templating lib
+// [x] initialize and configure Express app
+// [x] initialize templating lib
 // [ ] create home controller
 // [ ] bind routing
 // [ ] create layout
@@ -8,10 +8,29 @@
 
 const express = require('express');
 const hbs = require('express-handlebars');
+const { home } = require('./controllers/home');
+const { about } = require('./controllers/about');
+const { create } = require('./controllers/create');
+const { details } = require('./controllers/details');
+const { notFound } = require('./controllers/notFound');
 
 const app = express();
+
+app.engine(
+    'hbs',
+    hbs.create({
+        extname: '.hbs',
+    }).engine
+);
+app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('static'));
 
+app.get('/', home);
+app.get('/about', about);
+app.get('/create', create);
+app.get('/details/:id', details);
+
+app.all('*', notFound);
 app.listen(3000, () => console.log('Server started on port 3000.'));
