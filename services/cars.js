5e31e90a9658cb98f281af7bcf34300d79341e23
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-03-26 15:39:48 +0300

    price from to price filter added

--- services/cars.js
+++ services/cars.js
@@ -15,7 +15,7 @@ async function read() {
 
 async function write(data) {
     try {
-        await fs.writeFile(filePath, JSON.stringify(data));
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2));
     } catch (error) {
         console.error('Database write error');
         console.error(err);
@@ -23,11 +23,27 @@ async function write(data) {
     }
 }
 
-async function getAll() {
+async function getAll(query) {
     const data = await read();
-    return Object.entries(data).map(([id, value]) =>
+    let cars = Object.entries(data).map(([id, value]) =>
         Object.assign({}, { id }, value)
     );
+
+    if (query.search) {
+        cars = cars.filter((c) =>
+            c.name
+                .toLocaleLowerCase()
+                .includes(query.search.toLocaleLowerCase())
+        );
+    }
+    if (query.from) {
+        cars = cars.filter((c) => c.price >= Number(query.from));
+    }
+    if (query.to) {
+        cars = cars.filter((c) => c.price <= Number(query.to));
+    }
+
+    return cars;
 }
 
 async function getById(id) {
@@ -63,7 +79,7 @@ module.exports = () => (req, res, next) => {
     req.storage = {
         getAll,
         getById,
-        createCar
+        createCar,
     };
     next();
 };
