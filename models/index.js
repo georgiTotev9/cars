f2d1c87bfdcc57c7b511fad0f8871d178b19557f
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-04-04 21:16:11 +0300

    added accessories model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const Car = require('./Cars');
+require('./Cars');
+require('./Accessories');
 
 const connectionString = 'mongodb://127.0.0.1:27017/carbicle';
 
