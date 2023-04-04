85ab8eb155e642d3c9cd8c42dbc3766d111c5fa0
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-04-04 17:14:09 +0300

    added db connection

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Car = require('./Cars');
+
+const connectionString = 'mongodb://127.0.0.1:27017/carbicle';
+
+async function init() {
+    try {
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('!db connected!');
+
+        mongoose.connection.on('error', (err) => {
+            console.error('Db error');
+            console.error(err);
+        });
+    } catch (error) {
+        console.error('Error connecting to db');
+        process.exit(1);
+    }
+}
+
+module.exports = init;
