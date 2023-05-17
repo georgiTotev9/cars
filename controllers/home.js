75799a6a58d4656234e2e602650abb4403fda70e
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-05-18 00:25:34 +0300

    navigation fixed

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,5 @@
 module.exports = {
     async home(req, res) {
-        console.log(req.session);
         const cars = await req.storage.getAll(req.query);
 
         res.render('index', { cars, title: 'Carbicle', query: req.query });
