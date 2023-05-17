49ac685db11e584c18f24d2fc40396be3a1b31d1
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-05-18 01:59:26 +0300

    car owner field added

--- models/Cars.js
+++ models/Cars.js
@@ -1,10 +1,15 @@
-const { Schema, model } = require('mongoose');
+const {
+    Schema,
+    model,
+    Types: { ObjectId },
+} = require('mongoose');
 
 const carSchema = new Schema({
     name: { type: String, required: true },
     description: { type: String, default: '' },
     imageUrl: { type: String },
     price: { type: Number, min: 1, required: true },
+    owner: { type: ObjectId, ref: 'User' },
 });
 
 const Car = model('Car', carSchema);
