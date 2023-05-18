a2ed400f9b0f17efe0d24ddb9e20cc44324d7881
Georgi Dimov Totev <georgidimovtotev@gmail.com>
2023-05-18 17:30:10 +0300

    details buttons removed if no user

--- controllers/details.js
+++ controllers/details.js
@@ -3,10 +3,14 @@ module.exports = {
         const id = req.params.id;
         const car = await req.storage.getById(id);
 
+        if (req.session.user && req.session.user.id == car.owner) {
+            car.isOwner = true;
+        }
+
         if (car) {
             res.render('details', { title: `Carbicle - ${car.name}`, car });
         } else {
             res.redirect('/404');
         }
-    }
+    },
 };
